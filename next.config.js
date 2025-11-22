a2d8328c0d72d2445586522ae616ec0adb4c50b4
o-sallam <omar.sallam.dev@gmail.com>
2025-11-22 22:12:31 +0200

    Add Next.js export config and .nojekyll for GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// Next.js export config for static GitHub Pages site
+// Note: This file documents the build settings used to generate `out`
+// It is not used by GitHub Pages itself, but kept for reference.
+module.exports = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
